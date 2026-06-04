f06b8e28b13c140a0fed4f9f57e208f53a5acc05
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-06-04 16:19:21 +0800

    Successfully assigned instructed values to the first and second objects

---
+++ test_program.py
@@ -0,0 +1,15 @@
+from main import Fan
+
+class TestFan:
+    def fan_objects:
+        first_fan = Fan()
+        first_fan.set_speed(FAST)
+        first_fan.set_power_on(True)
+        first_fan.set_radius(10)
+        first_fan.set_color("yellow")
+
+        second_fan = Fan()
+        second_fan.set_speed(MEDIUM)
+        second_fan.set_power_on(False)
+        second_fan.set_radius(5)
+        second_fan.set_color("blue")
